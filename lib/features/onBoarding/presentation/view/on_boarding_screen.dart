9bb22c19824194790271efaa93f6ae73b976c388
PeshoOReda <drmafiaemail13@gmail.com>
2025-05-05 09:14:48 +0300

    feat(onboarding): implement onboarding screen with custom buttons and text fields

--- lib/features/onBoarding/presentation/view/on_boarding_screen.dart
+++ lib/features/onBoarding/presentation/view/on_boarding_screen.dart
@@ -1,1 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:tracking_flower_app/core/assets/app_colors.dart';
+import 'package:tracking_flower_app/core/assets/app_images.dart';
+import 'package:tracking_flower_app/core/utils/l10n/locale_keys.g.dart';
+import 'package:tracking_flower_app/features/onBoarding/presentation/view/custom_button.dart';
+import 'package:tracking_flower_app/features/onBoarding/presentation/view/text_field.dart'
+    as custom;
 
+class OnBoarding extends StatelessWidget {
+  const OnBoarding({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Center(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Image.asset(AppImages.onBoardingLogo, fit: BoxFit.cover),
+            ),
+          ),
+          const Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Text(
+              LocaleKeys.WelcomeToFloweryRiderApp,
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.w900,
+                color: AppColors.black,
+              ),
+              textAlign: TextAlign.start,
+            ),
+          ),
+          CustomButton(
+            label: LocaleKeys.Login,
+            onPressed: () {},
+            color: AppColors.pink,
+            textStyle: const TextStyle(color: AppColors.white, fontSize: 18),
+          ),
+          CustomButton(
+            label: LocaleKeys.ApplyNow,
+            onPressed: () {},
+            color: AppColors.white,
+            textStyle: const TextStyle(color: AppColors.black, fontSize: 18),
+          ),
+          const Spacer(),
+          const Padding(
+            padding: EdgeInsets.only(bottom: 18.0),
+            child: Center(
+              child: custom.TextField(
+                label: 'Version 1.0.0 (2000)',
+                textStyle: TextStyle(fontSize: 12, color: AppColors.gray),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:logger/logger.dart';
+import 'package:tracking_flower_app/features/onBoarding/presentation/view/on_boarding_screen.dart';
 
 import 'core/theme/app_theme.dart';
 import 'core/utils/app_starter.dart';
@@ -45,55 +46,12 @@ class MyApp extends StatelessWidget {
           title: 'Tracking Flower App',
           theme: AppTheme.appTheme,
           routes: AppRoutes.routes,
-          home: const MyHomePage(title: 'Flutter Demo Home Page'),
+          home: const OnBoarding(), // Replace with your initial screen
         );
       },
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
 
-  final String title;
 
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
-    );
-  }
-}
