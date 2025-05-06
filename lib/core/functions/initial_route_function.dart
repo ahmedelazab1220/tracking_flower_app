de2597b5e5f4c15b43fb8c2ddefc587b5c612bbf
Eslam <eslamm378@gmail.com>
2025-05-06 06:25:42 +0300

    refactor(routes): add onboarding and login routes with initial route logic

---
+++ lib/core/functions/initial_route_function.dart
@@ -0,0 +1,20 @@
+import 'package:injectable/injectable.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import '../utils/constants.dart';
+import '../utils/routes/app_routes.dart';
+
+@injectable
+class RouteInitializer {
+  final SharedPreferences sharedPreferences;
+
+  RouteInitializer({required this.sharedPreferences});
+
+  String? computeInitialRoute() {
+    var isRememberMe = sharedPreferences.getBool(Constants.isRememberMe);
+    if (isRememberMe != null && isRememberMe) {
+      return AppRoutes.loginRoute;
+    } else {
+      return AppRoutes.onBoardingRoute;
+    }
+  }
+}
--- lib/core/utils/routes/app_routes.dart
+++ lib/core/utils/routes/app_routes.dart
@@ -1,9 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:tracking_flower_app/features/onBoarding/presentation/view/on_boarding_screen.dart';
+
+import '../../../features/login/presentation/view/screens/login_screen.dart';
 
 class AppRoutes {
   AppRoutes._();
+
+  static const String onBoardingRoute = "/onBoarding";
   static const String loginRoute = "/login";
   static const String applyRoute = "/apply";
   static const String forgetPasswordRoute = "/forgetPassword";
-  static Map<String, Widget Function(BuildContext)> routes = {};
+  static Map<String, Widget Function(BuildContext)> routes = {
+    onBoardingRoute: (context) => const OnBoardingScreen(),
+    loginRoute: (context) => const LoginScreen(),
+  };
 }
