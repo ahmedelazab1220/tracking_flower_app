29228b2be220a048f0c364c8cb7a1c59a9805505
ahmedelazab1220 <ahmedelazab1220@gmail.com>
2025-05-08 19:45:38 +0300

    refactor(di.config.dart): update import paths and improve formatting for better readability

--- lib/core/utils/di/di.config.dart
+++ lib/core/utils/di/di.config.dart
@@ -30,12 +30,12 @@ import '../../../domain/auth/use_case/login_use_case.dart' as _i872;
 import '../../../domain/auth/usecase/forget_password_use_case.dart' as _i615;
 import '../../../domain/auth/usecase/reset_password_use_case.dart' as _i313;
 import '../../../domain/auth/usecase/verify_reset_code_use_case.dart' as _i684;
-import '../../../features/forget_password/presentation/view_model/email_verification/email_verification_cubit.dart'
-    as _i725;
+import '../../../features/forget_password/presentation/view_model/email_verification_cubit/email_verification_cubit.dart'
+    as _i296;
 import '../../../features/forget_password/presentation/view_model/forget_password_cubit/forget_password_cubit.dart'
     as _i3;
-import '../../../features/forget_password/presentation/view_model/reset_password/reset_password_cubit.dart'
-    as _i216;
+import '../../../features/forget_password/presentation/view_model/reset_password_cubit/reset_password_cubit.dart'
+    as _i61;
 import '../../../features/login/presentation/view_model/login_cubit.dart'
     as _i638;
 import '../../functions/initial_route_function.dart' as _i687;
@@ -48,12 +48,16 @@ import '../shared_preference_module.dart' as _i60;
 import '../validator/validator.dart' as _i468;
 
 extension GetItInjectableX on _i174.GetIt {
-  // initializes the registration of main-scope dependencies inside of GetIt
+// initializes the registration of main-scope dependencies inside of GetIt
   Future<_i174.GetIt> init({
     String? environment,
     _i526.EnvironmentFilter? environmentFilter,
   }) async {
-    final gh = _i526.GetItHelper(this, environment, environmentFilter);
+    final gh = _i526.GetItHelper(
+      this,
+      environment,
+      environmentFilter,
+    );
     final sharedPreferenceModule = _$SharedPreferenceModule();
     final secureStorageModule = _$SecureStorageModule();
     final loggerModule = _$LoggerModule();
@@ -65,74 +69,53 @@ extension GetItInjectableX on _i174.GetIt {
     gh.factory<_i468.Validator>(() => _i468.Validator());
     gh.singleton<_i28.ApiManager>(() => _i28.ApiManager());
     gh.lazySingleton<_i558.FlutterSecureStorage>(
-      () => secureStorageModule.storage,
-    );
+        () => secureStorageModule.storage);
     gh.lazySingleton<_i974.Logger>(() => loggerModule.loggerProvider);
     gh.lazySingleton<_i974.PrettyPrinter>(() => loggerModule.prettyPrinter);
     gh.singleton<_i649.BlocObserverService>(
-      () => _i649.BlocObserverService(gh<_i974.Logger>()),
-    );
-    gh.factory<_i687.RouteInitializer>(
-      () => _i687.RouteInitializer(
-        sharedPreferences: gh<_i460.SharedPreferences>(),
-      ),
-    );
+        () => _i649.BlocObserverService(gh<_i974.Logger>()));
+    gh.factory<_i687.RouteInitializer>(() => _i687.RouteInitializer(
+        sharedPreferences: gh<_i460.SharedPreferences>()));
     gh.lazySingleton<_i361.Dio>(
-      () => dioModule.provideDio(gh<_i558.FlutterSecureStorage>()),
-    );
-    gh.factory<_i1063.AuthLocalDataSource>(
-      () => _i757.AuthLocalDataSourceImpl(
-        gh<_i558.FlutterSecureStorage>(),
-        gh<_i460.SharedPreferences>(),
-      ),
-    );
+        () => dioModule.provideDio(gh<_i558.FlutterSecureStorage>()));
+    gh.factory<_i1063.AuthLocalDataSource>(() => _i757.AuthLocalDataSourceImpl(
+          gh<_i558.FlutterSecureStorage>(),
+          gh<_i460.SharedPreferences>(),
+        ));
     gh.factory<_i1064.AuthRetrofitClient>(
-      () => _i1064.AuthRetrofitClient(gh<_i361.Dio>()),
-    );
+        () => _i1064.AuthRetrofitClient(gh<_i361.Dio>()));
     gh.factory<_i774.AuthRemoteDataSource>(
-      () => _i173.AuthRemoteDataSourceImpl(gh<_i1064.AuthRetrofitClient>()),
-    );
-    gh.factory<_i1047.AuthRepo>(
-      () => _i15.AuthRepoImpl(
-        gh<_i774.AuthRemoteDataSource>(),
-        gh<_i1063.AuthLocalDataSource>(),
-        gh<_i28.ApiManager>(),
-      ),
-    );
+        () => _i173.AuthRemoteDataSourceImpl(gh<_i1064.AuthRetrofitClient>()));
+    gh.factory<_i1047.AuthRepo>(() => _i15.AuthRepoImpl(
+          gh<_i774.AuthRemoteDataSource>(),
+          gh<_i1063.AuthLocalDataSource>(),
+          gh<_i28.ApiManager>(),
+        ));
     gh.factory<_i615.ForgetPasswordUseCase>(
-      () => _i615.ForgetPasswordUseCase(gh<_i1047.AuthRepo>()),
-    );
+        () => _i615.ForgetPasswordUseCase(gh<_i1047.AuthRepo>()));
     gh.factory<_i313.ResetPasswordUseCase>(
-      () => _i313.ResetPasswordUseCase(gh<_i1047.AuthRepo>()),
-    );
+        () => _i313.ResetPasswordUseCase(gh<_i1047.AuthRepo>()));
     gh.factory<_i684.VerifyResetCodeUseCase>(
-      () => _i684.VerifyResetCodeUseCase(gh<_i1047.AuthRepo>()),
-    );
+        () => _i684.VerifyResetCodeUseCase(gh<_i1047.AuthRepo>()));
     gh.factory<_i872.LoginUsecase>(
-      () => _i872.LoginUsecase(gh<_i1047.AuthRepo>()),
-    );
-    gh.factory<_i725.EmailVerificationCubit>(
-      () => _i725.EmailVerificationCubit(
-        gh<_i684.VerifyResetCodeUseCase>(),
-        gh<_i615.ForgetPasswordUseCase>(),
-        gh<_i468.Validator>(),
-      ),
-    );
-    gh.factory<_i216.ResetPasswordCubit>(
-      () => _i216.ResetPasswordCubit(
-        gh<_i313.ResetPasswordUseCase>(),
-        gh<_i468.Validator>(),
-      ),
-    );
-    gh.factory<_i638.LoginCubit>(
-      () => _i638.LoginCubit(gh<_i872.LoginUsecase>(), gh<_i468.Validator>()),
-    );
-    gh.factory<_i3.ForgetPasswordCubit>(
-      () => _i3.ForgetPasswordCubit(
-        gh<_i615.ForgetPasswordUseCase>(),
-        gh<_i468.Validator>(),
-      ),
-    );
+        () => _i872.LoginUsecase(gh<_i1047.AuthRepo>()));
+    gh.factory<_i296.EmailVerificationCubit>(() => _i296.EmailVerificationCubit(
+          gh<_i684.VerifyResetCodeUseCase>(),
+          gh<_i615.ForgetPasswordUseCase>(),
+          gh<_i468.Validator>(),
+        ));
+    gh.factory<_i61.ResetPasswordCubit>(() => _i61.ResetPasswordCubit(
+          gh<_i313.ResetPasswordUseCase>(),
+          gh<_i468.Validator>(),
+        ));
+    gh.factory<_i638.LoginCubit>(() => _i638.LoginCubit(
+          gh<_i872.LoginUsecase>(),
+          gh<_i468.Validator>(),
+        ));
+    gh.factory<_i3.ForgetPasswordCubit>(() => _i3.ForgetPasswordCubit(
+          gh<_i615.ForgetPasswordUseCase>(),
+          gh<_i468.Validator>(),
+        ));
     return this;
   }
 }
